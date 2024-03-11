e26e2f207760dc2c6f34955acd90284dab04d3bb
jagiraldorestrepo <134088695+jagiraldorestrepo@users.noreply.github.com>
2024-03-11 18:46:57 -0500

    Add files via upload

---
+++ Figuras_geometricas/src/figuras_geometricas/Cuadrado.java
@@ -0,0 +1,19 @@
+
+package figuras_geometricas;
+
+
+public class Cuadrado {
+int lado; // Atributo que define el lado de un cuadrado
+
+    public Cuadrado(int lado) {
+        this.lado = lado;
+    }
+
+    double calcularArea() {
+        return lado*lado;
+    }
+ 
+    double calcularPerímetro() {
+        return (4*lado);
+    }
+}
---
+++ Figuras_geometricas/src/figuras_geometricas/TriánguloRectángulo.java
@@ -0,0 +1,38 @@
+
+package figuras_geometricas;
+
+public class TriánguloRectángulo {
+    int base; 
+    int altura; 
+    
+    public TriánguloRectángulo(int base, int altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+    double calcularArea() {
+        return (base * altura / 2);
+    }
+    double calcularPerímetro() {
+        return (base + altura + calcularHipotenusa());                      
+    }
+
+
+    double calcularHipotenusa() {
+        return Math.pow(base*base + altura*altura, 0.5);
+    }
+
+    void determinarTipoTriángulo() {
+        if ((base == altura) && (base == calcularHipotenusa()) && (altura == calcularHipotenusa())){
+            
+            System.out.println("Es un triángulo equilátero"); 
+        }
+
+        else if ((base != altura) && (base != calcularHipotenusa()) && (altura != calcularHipotenusa())){
+            System.out.println("Es un triángulo escaleno"); }
+
+        else{
+            System.out.println("Es un triángulo isósceles"); 
+    }
+
+    }
+}
